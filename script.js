32334140ff9608994eb1e76d5da74c6372d79463
= <28835813+ploop3@users.noreply.github.com>
2023-04-01 10:27:43 +0200

    Use promises to execute the API calls

--- script.js
+++ script.js
@@ -31,21 +31,50 @@ const renderCountry = function (data) {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryData = function (country) {
-  //--- Old school to use AJAX calls (new way is done through Promises)
-  const request = new XMLHttpRequest();
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  request.send(); //The request is being sent in the background and when the data arrives back, the load event will be called
+// const getCountryData = function (country) {
+//   //--- Old school to use AJAX calls (new way is done through Promises)
+//   const request = new XMLHttpRequest();
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   request.send(); //The request is being sent in the background and when the data arrives back, the load event will be called
 
-  // Get the result:
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    renderCountry(data);
+//   // Get the result:
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     renderCountry(data);
+//   });
+// };
+
+// //Since these are async calls, we can't control the order their respones will arrive
+// //which might cause the data to be shown in a different order
+// getCountryData("france");
+// getCountryData("usa");
+// getCountryData("germany");
+
+//--------------------------------
+// A solution to Excecute Async calls in sequence can be by chaining/nesting the callbacks, but it can cause a Callback Hell, where the code would be hard to undersatand and prone to bugs.
+
+//A better solution is to use Promises
+
+const getCountryData = function (country) {
+  //When executing the fetch funtion, to fetch data from an API for example, a Promise is returned immediately.
+  // The promise starts with pending state but eventually moves to fulfilled (once it has the data from the API) or rejected
+  const request = fetch(`https://restcountries.com/v3.1/name/${country}`);
+  console.log(request);
+
+  //Handle the fullfilled state
+  //We pass a callback function, which will be executed as soon as the promise is fullfilled
+  //The function will always have the response/result of the fullfilled promise(Object) as its argument
+  const responseJSONPromise = request.then(function (response) {
+    console.log(response);
+    //.json() is available in all response/result Objects from fetch, and it is also a promise.
+    return response.json();
+  });
+
+  responseJSONPromise.then(function (data) {
+    console.log(data); //Finally the data of the Countries API
+    renderCountry(data[0]);
   });
 };
 
-//Since these are async calls, we can't control the order their respones will arrive
-//which might cause the data to be shown in a different order
-getCountryData("france");
-getCountryData("usa");
+
 getCountryData("germany");
